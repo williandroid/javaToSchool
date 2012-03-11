58c6fdda536f704eab04d7a0b7d5eee4b9953a30
Tássio Auad <tassioauad@id.uff.br>
2012-03-11 00:12:11 -0300

    Criada a classe Aluno.

---
+++ src/adolebid/Aluno.java
@@ -0,0 +1,159 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adolebid;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Tassio Auad
+ */
+public class Aluno {
+    private int id;
+    private String nomeCompleto;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private ArrayList<Presencas> presencas = new ArrayList<Presencas>();
+    private ArrayList<Disciplinas> disciplinas = new ArrayList<Disciplinas>();
+    private ArrayList<Exame> exames = new ArrayList<Exame>();
+    private ArrayList<Trabalho> trabalhos = new ArrayList<Trabalho>();
+    
+    public Aluno(int id, String nomeCompleto, String email, String telefone, String endereco)
+    {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nomeCompleto
+     */
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    /**
+     * @param nomeCompleto the nomeCompleto to set
+     */
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the presencas
+     */
+    public ArrayList<Presencas> getPresencas() {
+        return presencas;
+    }
+
+    /**
+     * @param presencas the presencas to set
+     */
+    public void setPresencas(ArrayList<Presencas> presencas) {
+        this.presencas = presencas;
+    }
+
+    /**
+     * @return the disciplinas
+     */
+    public ArrayList<Disciplinas> getDisciplinas() {
+        return disciplinas;
+    }
+
+    /**
+     * @param disciplinas the disciplinas to set
+     */
+    public void setDisciplinas(ArrayList<Disciplinas> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    /**
+     * @return the exames
+     */
+    public ArrayList<Exame> getExames() {
+        return exames;
+    }
+
+    /**
+     * @param exames the exames to set
+     */
+    public void setExames(ArrayList<Exame> exames) {
+        this.exames = exames;
+    }
+
+    /**
+     * @return the trabalhos
+     */
+    public ArrayList<Trabalho> getTrabalhos() {
+        return trabalhos;
+    }
+
+    /**
+     * @param trabalhos the trabalhos to set
+     */
+    public void setTrabalhos(ArrayList<Trabalho> trabalhos) {
+        this.trabalhos = trabalhos;
+    }
+    
+}
