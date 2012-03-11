7714b9b596bc13131b1c21eb2e070683a4f93f7c
Tássio Auad <tassioauad@id.uff.br>
2012-03-11 00:18:18 -0300

    Adicionei a clsse Presencas.

--- src/adolebid/Presencas.java
+++ src/adolebid/Presencas.java
@@ -6,8 +6,61 @@ package adolebid;
 
 /**
  *
- * @author tassio
+ * @author Tássio Auad
  */
 class Presencas {
-    
+ 
+        private String data;
+        private boolean status;
+        private Disciplinas disciplina;
+
+    public Presencas(String data, boolean status, Disciplinas disciplina)
+    {
+        this.data = data;
+        this.status = status;
+        this.disciplina = disciplina;
+    }
+        
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the disciplina
+     */
+    public Disciplinas getDisciplina() {
+        return disciplina;
+    }
+
+    /**
+     * @param disciplina the disciplina to set
+     */
+    public void setDisciplina(Disciplinas disciplina) {
+        this.disciplina = disciplina;
+    }
+        
 }
