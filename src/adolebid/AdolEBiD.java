8a34c38bde1a82ad8aec3890a35ca905b434f4b4
Tássio Auad <tassioauad@id.uff.br>
2012-03-14 18:19:32 -0300

    Criei o menu de interação com o usuário e comecei a criar o método de cadastrar aluno na classe principal.

--- src/adolebid/AdolEBiD.java
+++ src/adolebid/AdolEBiD.java
@@ -4,6 +4,8 @@
  */
 package adolebid;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tassio
@@ -13,7 +15,51 @@ public class AdolEBiD {
     /**
      * @param args the command line arguments
      */
+    int idAluno = 000;
+    Scanner scan = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
+       
+       
+       int escolha;
+        
+        while(true)
+        {
+            System.out.println("Escolha uma das opcoes:");
+            System.out.println("[1] - Cadastrar");
+            System.out.println("[2] - Visualizar");
+            System.out.println("[3] - Sair");
+            escolha = scan.nextInt();
+            if(escolha == 3)
+                   break;
+        }
+    }
+    
+    public void cadastrarAluno()
+    {
+        int i, j;
+        String nomeCompleto;
+        String email;
+        String telefone;
+        String endereco;
+        
+        System.out.println("Entre com o nome completo do aluno:");
+        nomeCompleto = scan.nextLine();
+        System.out.println("Entre com o email do aluno:");
+        nomeCompleto = scan.nextLine();
+        System.out.println("Entre com o telefone do aluno:");
+        nomeCompleto = scan.nextLine();
+        System.out.println("Entre com o endereco do aluno:");
+        nomeCompleto = scan.nextLine();
+        Aluno a = new Aluno(idAluno, nomeCompleto, email, telefone, endereco);
+        
+        System.out.println("Voce deseja matricular esse aluno em qual disciplina?");
+
+                
+                    
+              
+        
     }
 }
