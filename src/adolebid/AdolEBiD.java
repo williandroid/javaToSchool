3fe94a1cfbbf0a81b7aff241c6a027a1bff6d458
Tássio Auad <tassioauad@id.uff.br>
2012-03-15 02:58:23 -0300

    Algumas melhorias na parte visual da tela de cadastro de aluno.

--- src/adolebid/AdolEBiD.java
+++ src/adolebid/AdolEBiD.java
@@ -18,6 +18,8 @@ public class AdolEBiD {
      */
     static public ArrayList<Curso> cursos = new ArrayList<Curso>();
     static Scanner scan = new Scanner(System.in);
+    static char esc = 27;  
+    static String clear = esc + "[2J"; //codigo ansi para limpar a tela  
     
     public static void main(String[] args) {
         int escolha;
@@ -30,6 +32,7 @@ public class AdolEBiD {
             switch(escolha)
             {
                 case 3:
+                    System.out.print(clear);  
                     cadastrarAluno();
                     break;
             }
@@ -42,7 +45,8 @@ public class AdolEBiD {
         int esc1;
         int opcDisc;
         Aluno aluno = new Aluno();
-        System.out.println("-------------------------------------------------");
+        System.out.println("Aluno Cadastrado com Sucesso.");
+        System.out.print(clear);  
         System.out.println("Deseja Matricular esse aluno em alguma disciplina?\n[0]-Sim\n[1]-Não\n\n");
         esc1 = scan.nextInt();
         if(esc1 == 0)
@@ -58,8 +62,31 @@ public class AdolEBiD {
             System.out.println("Opcao escolhida:");
             opcDisc = scan.nextInt();
             cursos.get(opcDisc/10).disciplinaDeId(opcDisc%10).setAlunos(aluno);
-        }
-        
+            System.out.println("Matriculado com sucesso.");
+            
+            while(esc1==0)
+            {
+               System.out.println("Deseja Matricular esse aluno em alguma outra disciplina?\n[0]-Sim\n[1]-Não\n\n");
+               esc1 = scan.nextInt();
+               System.out.print(clear); 
+                if(esc1 == 0)
+                { 
+                
+                    for(i = 0; i < cursos.size(); i++)
+                    {
+                        for(j = 0; j < cursos.get(i).getDisciplinas().size(); j++)
+                        {
+                            System.out.println("["+i+j+"] - "+ cursos.get(i).disciplinaDeId(j).getNome());
+                        }
 
+                    }
+                    System.out.println("Opcao escolhida:");
+                    opcDisc = scan.nextInt();
+                    cursos.get(opcDisc/10).disciplinaDeId(opcDisc%10).setAlunos(aluno);
+                    System.out.println("Matriculado com sucesso."); 
+                }
+           }
+       }
     }
+    
 }
